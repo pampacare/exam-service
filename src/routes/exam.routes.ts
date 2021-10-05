caef92066f8c337315893d585b8204050bd9e20f
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-10-05 15:06:11 -0300

    feat: listar todos os sintomas

--- src/routes/exam.routes.ts
+++ src/routes/exam.routes.ts
@@ -2,12 +2,15 @@
 import { Router } from 'express'
 import { FindByDogController } from '../controllers/exams/FindByDogController'
 import { RegisterExamController } from '../controllers/exams/RegisterExamController'
+import { ReturnSymptomsController } from '../controllers/symptoms/ReturnSymptomsController'
 const create = new RegisterExamController()
 const findByDogController = new FindByDogController()
+const returnSymptomsController = new ReturnSymptomsController()
 
 const examsRoutes = (router: Router): void => {
     router.get('/exam/:dogId', findByDogController.execute.bind(FindByDogController))
     router.post('/register-exam', create.execute.bind(RegisterExamController))
+    router.get('/exam/symptoms', returnSymptomsController.execute.bind(ReturnSymptomsController))
 }
 
 export { examsRoutes }
