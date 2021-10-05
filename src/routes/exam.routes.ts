e8f065021a1875359e4cc491efba2dd327d9e1be
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-10-05 15:52:05 -0300

    refactor: refinindo rota de busca por sintomas

--- src/routes/exam.routes.ts
+++ src/routes/exam.routes.ts
@@ -2,12 +2,15 @@
 import { Router } from 'express'
 import { FindByDogController } from '../controllers/exams/FindByDogController'
 import { RegisterExamController } from '../controllers/exams/RegisterExamController'
+import { GetSymptomsController } from '../controllers/symptom/GetSymptomsController'
+const getSymptomsController = new GetSymptomsController()
 const create = new RegisterExamController()
 const findByDogController = new FindByDogController()
 
 const examsRoutes = (router: Router): void => {
     router.get('/exam/:dogId', findByDogController.execute.bind(FindByDogController))
     router.post('/register-exam', create.execute.bind(RegisterExamController))
+    router.get('/symptoms', getSymptomsController.execute.bind(GetSymptomsController))
 }
 
 export { examsRoutes }
--- src/routes/symptomRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { GetSymptomsController } from '../controllers/symptom/GetSymptomsController'
-const getSymptomsController = new GetSymptomsController()
-
-const symptomRoutes = (router: Router): void => {
-  router.get('/symptoms', getSymptomsController.execute.bind(GetSymptomsController))
-}
-
-export { symptomRoutes }
