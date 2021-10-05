bc5c5fb0b799f8198b13af0a62c33e3485a405f1
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-10-05 15:44:27 -0300

    feat: listar todos os sintomas

--- src/routes/symptomRoutes.ts
+++ src/routes/symptomRoutes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express'
-import { GetSymptomsController } from '../controllers/dogs/GetSymptomsController'
+import { GetSymptomsController } from '../controllers/symptom/GetSymptomsController'
+const getSymptomsController = new GetSymptomsController()
 
 const symptomRoutes = (router: Router): void => {
-  router.get('/symptoms', (req, res) => {
-    const getSymptomsController = new GetSymptomsController()
-    getSymptomsController.execute(req,res)
-  })
+  router.get('/symptoms', getSymptomsController.execute.bind(GetSymptomsController))
 }
 
 export { symptomRoutes }
