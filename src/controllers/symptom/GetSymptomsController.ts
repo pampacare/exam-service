910c10b0c77ceee0dc89a75f6bb0f3fab49f07b6
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-10-05 15:47:47 -0300

    refactor: listar todos os sintomas

--- src/controllers/symptom/GetSymptomsController.ts
+++ src/controllers/symptom/GetSymptomsController.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
 import { Response } from 'express'
 import { prisma } from '../../services'
 
 class GetSymptomsController {
-  async execute (response: Response): Promise<Response> {
+  async execute(response: Response): Promise<Response> {
     try {
       const symptoms = await prisma.symptom.findMany()
-      return response.status(201).json(symptoms)
+      return response.status(201).json({
+        body: symptoms
+      })
     } catch (error: any) {
       return response.status(400).json({ message: error.message })
     }
